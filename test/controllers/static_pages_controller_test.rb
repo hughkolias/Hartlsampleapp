7f30927b6a7e47492689f8e4bda9061be8388860
Hugh Kolias <hughkolias@gmail.com>
2015-08-02 10:35:20 -0400

    add title for contact page and test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -28,7 +28,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get contact" do
     get :contact
     assert_response :success
-    # assert_select "title", "About | #{setup}"
+    assert_select "title", "Contact | #{setup}"
   end
 
 end
