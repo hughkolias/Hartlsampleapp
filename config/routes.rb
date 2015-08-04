c2f69528ca45cffd5cc5e6d4d5dbd62aad667fe7
Hugh Kolias <hughkolias@gmail.com>
2015-08-04 17:06:33 -0400

    add custom CSS.SCSS sheet, integration testing

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   root 'static_pages#home'
-  get 'static_pages/about'
-  get 'static_pages/help'
-  get 'static_pages/contact'
+  get 'about' => 'static_pages#about'
+  get 'help' => 'static_pages#help'
+  get 'contact' => 'static_pages#contact'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   # root 'application#hello'
